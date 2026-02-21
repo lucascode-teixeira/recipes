37ace3f658c997559bb8dc96b02d7e542c2f5f93
Lucas Teixeira <lucascode.teixeira@gmail.com>
2026-02-20 23:03:18 -0300

    there was no light

--- website/website/settings.py
+++ website/website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,4 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'front/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'front/static'),
+]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = '/'
